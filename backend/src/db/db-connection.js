fc4b8489dbb1c336ed54ab017ec1057c09ab776f
becky449 <rabnab12@gmail.com>
2024-03-16 08:01:44 +0300

    db connected

--- backend/src/db/db-connection.js
+++ backend/src/db/db-connection.js
@@ -1,66 +1,42 @@
 const dotenv = require('dotenv');
 dotenv.config();
-const mysql2 = require('mysql2');
+const { Pool } = require('pg');
 
 class DBConnection {
     constructor() {
-        this.db = mysql2.createPool({
+        this.db = new Pool({
             host: process.env.DB_HOST,
             user: process.env.DB_USER,
             password: process.env.DB_PASS,
-            database: process.env.DB_DATABASE
+            database: process.env.DB_DATABASE,
+            port: process.env.DB_PORT
         });
 
         this.checkConnection();
     }
 
     checkConnection() {
-        this.db.getConnection((err, connection) => {
+        this.db.connect((err, client, release) => {
             if (err) {
-                if (err.code === 'PROTOCOL_CONNECTION_LOST') {
-                    console.error('Database connection was closed.');
-                }
-                if (err.code === 'ER_CON_COUNT_ERROR') {
-                    console.error('Database has too many connections.');
-                }
-                if (err.code === 'ECONNREFUSED') {
-                    console.error('Database connection was refused.');
-                }
+                console.error('Error connecting to PostgreSQL database:', err);
+                return;
             }
-            if (connection) {
-                connection.release();
-            }
-            return
+            console.log('Connected to PostgreSQL database');
+            release();
         });
     }
 
     query = async (sql, values) => {
-        
         return new Promise((resolve, reject) => {
-            const callback = (error, result) => {
-                if (error) {
-                    reject(error);
+            this.db.query(sql, values, (err, result) => {
+                if (err) {
+                    reject(err);
                     return;
                 }
-                resolve(result);
-            }
-            // execute will internally call prepare and query
-            this.db.execute(sql, values, callback);
-        }).catch(err => {
-            const mysqlErrorList = Object.keys(HttpStatusCodes);
-            // convert mysql errors which in the mysqlErrorList list to http status code
-            err.status = mysqlErrorList.includes(err.code) ? HttpStatusCodes[err.code] : err.status;
-
-            throw err;
+                resolve(result.rows);
+            });
         });
     }
 }
-
-// like ENUM
-const HttpStatusCodes = Object.freeze({
-    ER_TRUNCATED_WRONG_VALUE_FOR_FIELD: 422,
-    ER_DUP_ENTRY: 409
-});
-
 
 module.exports = new DBConnection().query;
